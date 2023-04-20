a3b83a8c6fcdb490fc2c0c44b248d681af1f2080
Tech Hans (SJ HAN) <techhans2019@gmail.com>
2023-04-20 09:14:55 +0900

    Add files via upload

---
+++ day08/src/_230419/ArrayTest.java
@@ -0,0 +1,37 @@
+package _230419;
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] score = {10, 20, 30, 40, 50};
+		System.out.println("[DEBUG] score :"+score[0]);
+		System.out.println("[DEBUG] score :"+score[1]);
+		System.out.println("[DEBUG] score :"+score[2]);
+		System.out.println("[DEBUG] score :"+score[3]);
+		System.out.println("[DEBUG] score :"+score[4]);
+
+		int[] score2 = new int[] {};
+//		score2[0] = 1;
+//		score2[1] = 2;
+
+//		System.out.println("[DEBUG] score2 :"+score2[0]);
+//		System.out.println("[DEBUG] score2 :"+score2[1]);
+//		System.out.println("[DEBUG] score2 :"+score2[2]);
+//		System.out.println("[DEBUG] score2 :"+score2[3]);
+//		System.out.println("[DEBUG] score2 :"+score2[4]);
+		
+		int[] score3 = new int[] {10, 20, 30, 40, 50};
+		System.out.println("[DEBUG] score3 :"+score3[0]);
+		System.out.println("[DEBUG] score3 :"+score3[1]);
+		System.out.println("[DEBUG] score3 :"+score3[2]);
+		System.out.println("[DEBUG] score3 :"+score3[3]);
+		System.out.println("[DEBUG] score3 :"+score3[4]);
+		
+//		int[] score4 = new int[];     // error
+		int[] score5 = new int[5];
+		
+		
+	}
+
+}
